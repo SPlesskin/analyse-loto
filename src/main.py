76a491551da2a5625799d69954a763afda66ea66
SPlesskin <59414715+SPlesskin@users.noreply.github.com>
2025-12-14 01:15:35 +0100

    Add DATA_DIR constant

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+from pathlib import Path
+
 def main():
-    print("Hello, World!")
+    # Récupère le chemin absolu du fichier actuel (main.py)
+    BASE_DIR = Path(__file__).parent.parent
+
+    # Construit le chemin vers le répertoire data/
+    DATA_DIR = BASE_DIR / "data"
+
+    # Vérifie que le répertoire en question existe bien
+    if DATA_DIR.is_dir():
+        print("Le répertoire data/ a été trouvé.")
+    else:
+        print("Le répertoire data/ est introuvable.")
 
 if __name__ == "__main__":
     main()
